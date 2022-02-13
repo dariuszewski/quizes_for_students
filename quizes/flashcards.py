9d7936851d69fac6ba89a83f692d90ad50fa79b3
dariuszewski <dariuszewski32@gmail.com>
2022-02-13 18:02:17 +0100

    display cards, take test, add card

--- quizes/flashcards.py
+++ quizes/flashcards.py
@@ -1,12 +1,14 @@
-from operator import index
-from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from hmac import new
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for, flash, session
 
+import json
+import random
 
 from model import Api
 
 app = Flask(__name__)
 
-counter = 0
+app.secret_key='secret'
 
 @app.route("/")
 def welcome():
@@ -18,3 +20,70 @@ def welcome():
 def api(topic):
     return jsonify(Api(topic).data)
 
+
+@app.route("/cards/<topic>/")
+def cards(topic):
+    cards = Api(topic).data
+    return render_template("cards.html", topic=topic, cards=cards)
+
+@app.route("/quiz/<topic>/", methods=["GET", "POST"])
+def quiz(topic):
+    if request.method == "POST":
+        # check quiz and return it as flash message
+        quiz = session["quiz"]
+        user_answers = request.form
+        result = 0
+        current_question = 0
+
+        for question in quiz:
+            if question['answer'] == user_answers[str(current_question)]: # some weird data structure here
+                result += 1
+            current_question += 1 
+
+        topic_name = topic.replace('_', ' ').title()
+
+        flash(f'Your result of {topic_name} quiz is {result}/9.')        
+        return redirect(url_for('welcome'))
+
+
+    quiz = Api(topic).generate_quiz()
+    session['quiz'] = quiz
+    return render_template("quiz.html", topic=topic, quiz=quiz)
+
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+
+        all_answers = [
+            request.form['correct_answer'], 
+            request.form['incorrect_answer1'], 
+            request.form['incorrect_answer2'], 
+            request.form['incorrect_answer3']
+            ]
+        random.shuffle(all_answers)
+
+        new_card = {
+            "question": request.form['question'],
+            "answer": request.form['correct_answer'],
+            "answers": all_answers
+        }
+
+        api = Api(request.form['topic'])
+        api.data.append(new_card)
+        api.save_api()
+        flash(f'New card was successfully added to {api.name} dataset.')
+        return redirect(url_for('welcome'))
+
+    return render_template("add_card.html")
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search(phrase=None):
+    if request.method == "POST":
+        phrase = request.form['question']
+        result = Api('system_administration').find_phrase(phrase)
+
+        return render_template("search.html", result=result)
+    else:
+        return render_template(
+            "search.html")
--- quizes/model.py
+++ quizes/model.py
@@ -1,5 +1,5 @@
 import json
-
+from random import sample
 
 class Api:
 
@@ -8,12 +8,30 @@ class Api:
         with open(f"apis/{name}.json") as file:
             self.data = json.load(file)
 
-    def save_db(self):
+    def save_api(self):
         with open(f"apis/{self.name}.json", "w") as file:
             return json.dump(self.data, file)
 
+    def generate_quiz(self):
+        quiz = [self.data[i] for i in sample(range(len(self.data)-1), 9)]
+        return quiz
 
-#adsk = Entity('system_administration')
+    def find_phrase(self, phrase):
+        # this function is not used yet
+        result = []
+        for card in self.data:
+            if phrase.lower() in str(card).lower():
+                result.append(card)
+        print(result)
+        print(len(result))
+        return result
+
+
+
+
+# adsk = Api('system_administration')
+# adsk.generate_quiz()
+# adsk.find_phrase('SSH')
 #new_question = {'question': 'Enjoy your food', 'answer': 'Smacznego'}
 #adsk.db.append(new_question)
 #adsk.save_db()
